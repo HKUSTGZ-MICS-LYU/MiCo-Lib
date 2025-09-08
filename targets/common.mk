28a2a2447f2af21eeec58815c70d7cdd9f93f0f3
Jzjerry <jzjerry@foxmail.com>
2025-09-08 14:49:12 +0800

    refactor & mico64 briefly tested

---
+++ targets/common.mk
@@ -0,0 +1,13 @@
+
+# Software Optimization
+ifneq ($(filter unroll, $(OPT)),)
+	MICO_SOURCES += $(wildcard $(MICO_DIR)/src/mico_unrolled/*.c)
+endif
+
+ifneq ($(filter gcc-unroll, $(OPT)),)
+	CFLAGS += -funroll-all-loops
+endif
+
+ifneq ($(filter im2col, $(OPT)),)
+	MICO_SOURCES += $(wildcard $(MICO_DIR)/src/im2col_conv2d/*.c)
+endif
