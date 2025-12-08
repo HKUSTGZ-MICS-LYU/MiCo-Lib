d1b836448689720699064c8129a0cea79d473c1a
Jzjerry <jzjerry@foxmail.com>
2025-12-08 14:21:46 +0800

    refactor: move riscv optimized sw from target to src

--- targets/common.mk
+++ targets/common.mk
@@ -12,6 +12,10 @@ ifneq ($(filter im2col, $(OPT)),)
 	MICO_SOURCES += $(wildcard $(MICO_DIR)/src/im2col_conv2d/*.c)
 endif
 
+ifneq ($(filter riscv, $(OPT)),)
+	MICO_SOURCES += $(wildcard $(MICO_DIR)/src/riscv/*.c)
+endif
+
 ifneq ($(filter ref, $(OPT)),)
 	CFLAGS += -DREF
 endif
